acb5f5bf949d8b0b76c538efd2b8079cdb8ddcd6
globalavocado <globalavocado@gmail.com>
2016-05-13 23:35:20 +0100

    form to add tasks, using event listeners

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -6,6 +6,27 @@ import './body.html';
 
 Template.body.helpers({
   todos() {
-  	return Todos.find({});
+  	//show nearest tasks at the top
+  	return Todos.find({}, { sort: { createdAt: -1 } });
   },
+});
+
+Template.body.events({
+	'submit .new-task'(event) {
+		// prevent default browser form submit
+		event.preventDefault();
+
+		// get value from form element
+		const target = event.target;
+		const name = target.name.value;
+
+		// insert a task into the collection
+		Todos.insert({
+			name,
+			createdAt: new Date(), // current time
+		});
+
+		// clear form
+		target.name.value = '';
+	},
 });
